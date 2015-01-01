7b9eeb6c37112cf0610b641c62c604c4ce237042
Sébastien Helleu <flashcode@flashtux.org>
2015-01-01 09:24:35 +0100

    Update copyright dates

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2003-2014 Sébastien Helleu <flashcode@flashtux.org>
+# Copyright (C) 2003-2015 Sébastien Helleu <flashcode@flashtux.org>
 #
 # This file is part of WeeChat.org.
 #
