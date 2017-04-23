25bc86a1346eef69ff13eb66485f920b3e743bab
Sébastien Helleu <flashcode@flashtux.org>
2017-04-23 12:32:37 +0200

    Update French translations

--- weechat/download/_i18n_security.py
+++ weechat/download/_i18n_security.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This file is auto-generated after changes in database, DO NOT EDIT!
 
 from django.utils.translation import gettext_lazy
@@ -5,8 +6,10 @@ from django.utils.translation import gettext_lazy
 _i18n_download_security = [
     gettext_lazy("/set irc.network.colors_receive off"),
     gettext_lazy("Buffer overflow when decoding IRC colors in strings."),
+    gettext_lazy("Buffer overflow when removing quotes in DCC filename."),
     gettext_lazy("Buffer overflows in build of strings."),
     gettext_lazy("Crash when receiving special chars in IRC messages."),
+    gettext_lazy("Create a trigger (with WeeChat >= 1.1): /trigger add irc_dcc_quotes modifier \"irc_in_privmsg\" \"${arguments} =~ ^[^ ]+ :${\\x01}DCC SEND ${\\x22} \" \"/.*//\""),
     gettext_lazy("Do not use command /notify with nicks containing formatting chars like \"%\"."),
     gettext_lazy("Do not use irc protocol in relay plugin."),
     gettext_lazy("Missing verifications in SSL certificate, which allows man-in-the-middle attackers to spoof an SSL chat server via an arbitrary certificate."),
