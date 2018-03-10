109d45b048174ea98a6271b321d5cb380513e916
Sébastien Helleu <flashcode@flashtux.org>
2018-03-10 21:29:34 +0100

    Move page download/interfaces to about/interfaces

--- weechat/download/urls.py
+++ weechat/download/urls.py
@@ -35,9 +35,6 @@ from weechat.download.views import (
 
 urlpatterns = [
     url(r'^$', view_packages, name='download'),
-    url(r'^interfaces/$',
-        TemplateView.as_view(template_name='download/interfaces.html'),
-        name='download_interfaces'),
     url(r'^debian/$', view_repos, {'active': 'active'},
         name='download_debian'),
     url(r'^debian/(?P<active>(active|all))/$', view_repos,
