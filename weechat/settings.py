09b25b689f1a7de303cff1057b3f85324b794bcb
Sébastien Helleu <flashcode@flashtux.org>
2018-11-22 23:28:54 +0100

    Enable CSRF middleware

--- weechat/settings.py
+++ weechat/settings.py
@@ -105,7 +105,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
