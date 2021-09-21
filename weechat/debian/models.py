e8e7b3c2d92671212fe88093d0a5e6c0b67d3a37
KaiserBarbarossa <kaiser.barbarossa@yandex.com>
2021-09-21 20:11:20 +0200

    Switch from /usr/share/keyrings to /usr/share/local/keyrings

--- weechat/debian/models.py
+++ weechat/debian/models.py
@@ -76,20 +76,20 @@ class Repo(models.Model):
     def apt_url(self):
         """
         Return the URL to use with apt for binary packages, for example:
-        "deb [signed-by=/usr/share/keyrings/weechat-archive-keyring.gpg]
+        "deb [signed-by=/usr/local/share/keyrings/weechat-archive-keyring.gpg]
         https://weechat.org/debian sid main".
         """
-        return (f'deb [signed-by=/usr/share/keyrings/weechat-archive-keyring.gpg]'
-                f'{self.url} {self.version.codename} main')
+        return (f'deb [signed-by=/usr/local/share/keyrings/weechat-archive-'
+                f'keyring.gpg] {self.url} {self.version.codename} main')
 
     def apt_url_src(self):
         """
         Return the URL to use with apt for sources packages, for example:
-        "deb-src [signed-by=/usr/share/keyrings/weechat-archive-keyring.gpg]
-        https://weechat.org/debian sid main".
+        "deb-src [signed-by=/usr/local/share/keyrings/weechat-archive-
+        keyring.gpg] https://weechat.org/debian sid main".
         """
-        return (f'deb-src [signed-by=/usr/share/keyrings/weechat-archive-keyring.gpg]'
-                f'{self.url} {self.version.codename} main')
+        return (f'deb-src [signed-by=/usr/local/share/keyrings/weechat-archive-'
+                f'keyring.gpg] {self.url} {self.version.codename} main')
 
     class Meta:
         """Sort Repos by priority."""
