f025352e81e6f345bc5082ad9fd3c1d4cd1b1668
Sébastien Helleu <flashcode@flashtux.org>
2026-08-04 23:09:18 +0200

    Remove redundant license text from headers

--- bin/release.sh
+++ bin/release.sh
@@ -4,23 +4,6 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# This file is part of WeeChat.org.
-#
-# WeeChat.org is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# WeeChat.org is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with WeeChat.org.  If not, see <https://www.gnu.org/licenses/>.
-#
-
-#
 # Manage release: add/update a release, set stable/devel version.
 #
 # Syntax:
@@ -41,7 +24,6 @@
 #   ./release.sh add weechat 4.0.0
 #   ./release.sh stable weechat 4.0.0
 #   ./release.sh devel weechat 4.1.0-dev
-#
 
 set -o errexit
 
